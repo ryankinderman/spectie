3a65c104069d53510a6275a62e8ca58f02e47df9
Ryan Kinderman <ryan@kinderman.net>
2009-04-27 15:04:52 -0500

    - made RailsExampleGroup inherit from RSpec ExampleGroup instead of including individual modules - added support for 'scenario' method for creating examples

--- lib/rspec_integration_testing/rails_example_group.rb
+++ lib/rspec_integration_testing/rails_example_group.rb
@@ -1,7 +1,13 @@
 module RspecIntegrationTesting
-  class RailsExampleGroup
-    extend Spec::Example::ExampleGroupMethods
-    include Spec::Example::ExampleMethods
+  class RailsExampleGroup < Spec::Example::ExampleGroup
+
+    class << self
+
+      def scenario(description, options={}, backtrace=nil, &implementation)
+        example(description, options={}, backtrace=nil, &implementation)
+      end
+
+    end
 
     Spec::Example::ExampleGroupFactory.register(:integration, self)
   end
--- spec/rspec_integration_testing/rails_example_group_spec.rb
+++ spec/rspec_integration_testing/rails_example_group_spec.rb
@@ -1,7 +1,34 @@
 require File.expand_path(File.dirname(__FILE__) + "/../spec_helper")
 
-describe RspecIntegrationTesting::RailsExampleGroup do
+module RspecIntegrationTesting
+
+  describe RailsExampleGroup do
+
+    it "is a subclass of RSpec's ExampleGroup" do
+      RspecIntegrationTesting::RailsExampleGroup.superclass.should == Spec::Example::ExampleGroup
+    end
+
+    it "supports 'scenario' keyword for creating an example" do
+      example_group = Class.new(RailsExampleGroup)
+      example_ran_as_scenario = false
+      example_group.scenario "As a user, I want to make a series of requests for our mutual benefit" do
+        example_ran_as_scenario = true
+      end
+
+      example_group.run(::Spec::Runner::Options.new(StringIO.new, StringIO.new))
+
+      example_ran_as_scenario.should be_true
+    end
+
+  end
 
 
+  class ExampleSupportsScenarios < RailsExampleGroup
+    describe RailsExampleGroup, "supporting scenarios"
+
+    scenario "As a user of a system, I would like to make a series of requests against your system" do
+    end
+
+  end
 
 end
