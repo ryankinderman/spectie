47bb3f0c5e5899d0634aeda7ed5f73176aa78400
Ryan Kinderman <ryan@kinderman.net>
2009-07-05 10:36:38 -0500

    moved RSpec method_missing interop patch from specs to lib for use outside the examples, and removed dependency on the RSpec IntegrationExampleGroup, since it isn't available in the latest stable branch

--- lib/rspec_integration_testing.rb
+++ lib/rspec_integration_testing.rb
@@ -1,5 +1,24 @@
 require "rspec_integration_testing/story_example_group_methods"
+###################################################################################
+# Preserve method_missing from rspec, which supports the general 
+# be_ and have_ predicate matchers. This method gets overridden for
+# Rails integration tests by ActionController::IntegrationTest for
+# session delegation.
+require "spec/interop/test/unit/testcase"
+Test::Unit::TestCase.class_eval do 
+  alias_method :method_missing_from_rspec, :method_missing
+end
 
+require "action_controller/integration"
+ActionController::IntegrationTest.class_eval do
+  alias_method :method_missing_from_rails, :method_missing
+  def method_missing(*args, &block)
+    method_missing_from_rspec(*args, &block)
+  rescue NameError
+    method_missing_from_rails(*args, &block)
+  end
+end
+###################################################################################
 begin
   require "spec/rails"
 rescue LoadError
